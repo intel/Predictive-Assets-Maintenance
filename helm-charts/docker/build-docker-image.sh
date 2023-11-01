e17a0fb670ad9673ebc1fee6a66d0ac02abcdd8d
binbin Deng <108676127+plusbang@users.noreply.github.com>
2023-11-01 17:59:19 +0800

    Update docker image build (#13)
    * update

--- helm-charts/docker/build-docker-image.sh
+++ helm-charts/docker/build-docker-image.sh
@@ -1,11 +1,2 @@
-export HTTP_PROXY_HOST=your_http_proxy_host
-export HTTP_PROXY_PORT=your_http_proxy_port
-export HTTPS_PROXY_HOST=your_https_proxy_host
-export HTTPS_PROXY_PORT=your_https_proxy_port
-
-
-sudo docker build \
-   --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
-   --build-arg https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT \
-   -t intelanalytics/bigdl:asset-maintenance-ubuntu-20.04 .
+sudo docker build -t intelanalytics/bigdl:asset-maintenance-ubuntu-20.04 .
 
