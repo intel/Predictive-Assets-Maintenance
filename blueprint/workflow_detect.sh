e72786c184a09376e50178c64f3e21664b21b402
plusbang <binbin1.deng@intel.com>
2023-11-01 17:56:46 +0000

    update

--- blueprint/workflow_detect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -r $INPUT_FORECAST/output/ /cnvrg/
-rm -rf /workspace/output
-ln -s /cnvrg/output /workspace/
-ln -s /cnvrg/anomaly-detect.py /workspace/anomaly-detect.py
-cd /workspace
-python anomaly-detect.py
